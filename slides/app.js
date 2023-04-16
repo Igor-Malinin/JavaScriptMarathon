bfa9ac9c2475f18361224d8bc23dff97c796b6b8
Igor-Malinin <86961903+Igor-Malinin@users.noreply.github.com>
2023-04-16 19:29:38 +0400

    Update app.js

--- slides/app.js
+++ slides/app.js
@@ -1,11 +1,5 @@
 const slides = document.querySelectorAll(".slide");
 
-// slides.map((slide) => {
-// 	slide.addEventListener('click', () => {
-// 		slide.classList.add('active');
-// 	})
-// });
-
 slides.forEach((slide) => {
 	slide.addEventListener('click', () => {
 	clearActiveClasses();
